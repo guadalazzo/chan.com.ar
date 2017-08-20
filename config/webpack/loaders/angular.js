30076c69cb9764dbed1aae561bd18cfa947b9daf
guadalazzo <guadalazzo@est.frba.utn.edu.ar>
2017-08-20 15:30:47 -0300

    added templateUrl

--- config/webpack/loaders/angular.js
+++ config/webpack/loaders/angular.js
@@ -1,4 +1,12 @@
+const {env} = require('../configuration.js');
+isProd = env.NODE_ENV === 'production';
 module.exports = {
-  test: /.ts$/,
-  loader: 'ts-loader'
-}
+    test: /\.ts$/,
+    use: [
+        {
+            loader: 'awesome-typescript-loader',
+            options: { useCache: !isProd }
+        },
+        'angular2-template-loader'
+    ]
+};
