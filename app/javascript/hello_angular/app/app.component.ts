9b9adef697b05f4c4ebb87a9ac290a94de551d2a
guadalazzo <guadalazzo@est.frba.utn.edu.ar>
2017-08-20 16:01:23 -0300

    Fixed sass

--- app/javascript/hello_angular/app/app.component.ts
+++ app/javascript/hello_angular/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'hello-angular',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+
 })
 export class AppComponent {
   name = 'Angular!';
