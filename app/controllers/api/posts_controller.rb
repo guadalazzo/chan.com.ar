5e8b18e043714d9b73ed1a9fadde4776e933c81c
guadalazzo <guadalazzo@est.frba.utn.edu.ar>
2017-08-21 14:29:13 -0300

    added active admin

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -2,7 +2,7 @@ class Api::UsersController < ApplicationController
 	include Wor::Paginate
 
 	def index
-		@users = User.all
-		render_paginated @users
+		@posts = Post.all
+		render_paginated @posts
 	end
 end
