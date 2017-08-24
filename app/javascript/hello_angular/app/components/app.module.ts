0f6ec15e14fc0d12f07872a8526d3f8be21fcfed
hugo <hdf1986.6700@gmail.com>
2017-08-23 23:34:06 -0300

    migrate to ngx bootstrap

--- app/javascript/hello_angular/app/components/app.module.ts
+++ app/javascript/hello_angular/app/components/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { Carousel} from './carousel.component';
 import {PostComponent}from './post.component' ;
 import  PostsService  from '../services/posts.service';
@@ -16,7 +16,7 @@ import  PostsService  from '../services/posts.service';
   imports: [
     BrowserModule,
     HttpModule,
-    NgbModule.forRoot()
+    CarouselModule.forRoot()
   ],
   providers: [PostsService],
   bootstrap: [AppComponent]
--- app/javascript/hello_angular/app/components/carousel.component.ts
+++ app/javascript/hello_angular/app/components/carousel.component.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
 
 @Component({
-  selector: 'carousel',
+  selector: 'lupe-carousel',
   templateUrl: './carousel.component.html'
 })
 export class Carousel {
