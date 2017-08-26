0eb494f34e62e4db9e659aab0e2d527a4a68d48d
guadalazzo <guadalazzo@est.frba.utn.edu.ar>
2017-08-26 01:37:59 -0300

    Animated p elem

--- app/javascript/hello_angular/app/components/app.component.ts
+++ app/javascript/hello_angular/app/components/app.component.ts
@@ -3,6 +3,7 @@ import PostsService from '../services/posts.service';
 import Post from '../models/post.model';
 import { PostComponent } from './post.component';
 import {Carousel} from './carousel.component'
+import WOW from 'wow.js';
 @Component({
   selector: 'app',
   templateUrl: './app.component.html',
@@ -16,7 +17,8 @@ export class AppComponent {
 	total_count: number;
 	constructor(private postsService: PostsService){}
 	ngOnInit(){
-		this.postsService.getPosts().subscribe(response=> {
+		new WOW().init();	
+			this.postsService.getPosts().subscribe(response=> {
 			this.total_count=response.total_count
 			this.total_pages=response.total_pages
 			this.posts = response.page
